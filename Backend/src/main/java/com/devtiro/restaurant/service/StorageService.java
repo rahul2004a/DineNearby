9179432879a3d7ba7000f18517b4fecf23fdc0e3
rahul2004a <kumar.rahul.2017354@gmail.com>
2025-06-08 15:48:31 +0530

    Storing and Retriveing file

---
+++ Backend/src/main/java/com/devtiro/restaurant/service/StorageService.java
@@ -0,0 +1,11 @@
+package com.devtiro.restaurant.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface StorageService {
+    String store(MultipartFile file, String filename);
+    Optional<Resource> loadAsResource(String id);
+}
